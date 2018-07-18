56760919d58d38ba086cd970566a1ab0a6033d1c
Zach Smith <zachsmith.sa@gmail.com>
2018-07-18 14:58:45 +0200

    removed code for a simple duplex service

--- DuplexWCF.Client/DuplexWCF.Client/Program.cs
+++ DuplexWCF.Client/DuplexWCF.Client/Program.cs
@@ -10,17 +10,12 @@ namespace Client
         {
             Console.WriteLine("Client Started");
 
-            // Test ChannelFactory
-            NetTcpBinding binding = new NetTcpBinding();
-            binding.Name = "netTcpBindingBasic"; // Note this is actually the name of a binding, not a service as defined in the host app.config
-            EndpointAddress address = new EndpointAddress("net.tcp://localhost:3123/Service");
-            IService channel = new ChannelFactory<IService>(binding, address).CreateChannel();
-            channel.HelloWorld("ChannelFactory");
-
             // Test DuplexChannelFactory
-            NetTcpBinding duplexBinding = new NetTcpBinding();
-            duplexBinding.Name = "netTcpBindingBasic"; // Note this is actually the name of a binding, not a service as defined in the host app.config
-            EndpointAddress duplexAddress = new EndpointAddress("net.tcp://localhost:3123/DuplexService");
+            NetTcpBinding duplexBinding = new NetTcpBinding
+            {
+                Name = "netTcpBinding"
+            };
+            EndpointAddress duplexAddress = new EndpointAddress("net.tcp://localhost:3124/DuplexService");
             DuplexService serviceWithCallbackContract = new DuplexService();
             InstanceContext instanceContext = new InstanceContext(serviceWithCallbackContract);
             DuplexChannelFactory<IDuplexService> factory = new DuplexChannelFactory<IDuplexService>(instanceContext, duplexBinding, duplexAddress);
--- DuplexWCF.Host/Program.cs
+++ DuplexWCF.Host/Program.cs
@@ -12,18 +12,18 @@ namespace DuplexWCF.Host
         static void Main(string[] args)
         {
             // Create the normal channel factory
-            ServiceHost host = new ServiceHost(typeof(Service.Service));
-            host.Open();
-            Console.WriteLine("Unidirectional service started");
+            //ServiceHost host = new ServiceHost(typeof(Service.Service));
+            //host.Open();
+            //Console.WriteLine("Unidirectional service started");
 
             // Create the duplex channel factory
             ServiceHost duplexHost = new ServiceHost(typeof(Service.DuplexService));
             duplexHost.Open();
             Console.WriteLine("Duplex service started");
 
-            WebServiceHost webHost = new WebServiceHost(typeof(Service.WebService));
-            webHost.Open();
-            Console.WriteLine("Web host started");
+            //WebServiceHost webHost = new WebServiceHost(typeof(Service.WebService));
+            //webHost.Open();
+            //Console.WriteLine("Web host started");
 
             // Keep service alive
             Console.ReadLine();
